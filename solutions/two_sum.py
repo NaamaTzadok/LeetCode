7382d8d6968d2ea320745aa85a2f7369421e2eb4
mac <mac@macs-MacBook-Pro.local>
2026-05-06 16:41:05 +0300

    soll q num 1

---
+++ solutions/two_sum.py
@@ -0,0 +1,20 @@
+# Question number: 1
+# Level: esay
+# Author: Naama Tzadok
+# Date: 26.4.2026
+
+
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {x: i for i, x in enumerate(nums)}
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in indices.keys() and indices.get(x) != i:
+                return [i, indices.get(x)]
+            
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
