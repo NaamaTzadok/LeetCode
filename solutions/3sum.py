6209eb98c71d7c2f6ddd2c12d1621da5521e4005
NaamaTzadok <naamatzadok10@gmail.com>
2026-05-07 09:44:22 +0300

    Apr 12, 2026 10:59

---
+++ solutions/3sum.py
@@ -0,0 +1,34 @@
+# Question number: 9
+# Level: easy
+# Author: Naama Tzadok
+# Date: Apr 12, 2026 10:59
+
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = set()
+        nums.sort()
+        for i in range(len(nums)-2):
+            j = i + 1
+            k = len(nums)-1
+            
+            while j < k:
+                x, y, z = nums[i], nums[j], nums[k]
+                s = x + y + z
+                if s == 0:
+                    res.add(tuple([x, y, z]))
+                    j += 1
+                    k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return list(res)
+    
+# Time Complexity: O(n^2)
+# Space Complexity: O(n^2)
