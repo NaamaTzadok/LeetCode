631bd420a50ce8f192942c9d754b160a695476c4
NaamaTzadok <naamatzadok10@gmail.com>
2026-07-02 22:50:07 +0300

    Jul 02, 2026 22:37

---
+++ solutions/return_length_of_arguments_passed.js
@@ -0,0 +1,19 @@
+// Question number: 2703
+// Level: easy
+// Author: Naama Tzadok
+// Date: Jul 02, 2026 22:37
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
